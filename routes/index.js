3b945d60b40b7ddfaae5b718bf01ed515d91d801
Reed <reedbenjamin4@gmail.com>
2024-02-13 12:49:47 -0500

    Created modularized routes, connected to and write to db.json, added helpers folder for creating unique ID's and writing and appending to file functions

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
+const router = require('express').Router()
 
 // Import our files containing our routes
-const notesRouter = require('./notes');
+const notesRouter = require('./notes')
 
-router.use('/notes', notesRouter);
+router.use('/notes', notesRouter)
 
 module.exports = router;
--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,35 @@
 const notesRouter = require('express').Router()
+const fs = require('fs')
+const util = require('util')
+const uuid = require('../helpers/uuid')
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils')
 
 
 notesRouter.get('/', (req, res) => {
-    console.log('Something')
+    console.info(`${req.method} request received for note`)
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 })
 
-
 notesRouter.post('/', (req, res) => {
+    console.info(`${req.method} request received to add a note`)
     console.log(req.body)
+  
+    const { title, text } = req.body
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuid()
+      };
+  
+      readAndAppend(newNote, './db/db.json')
+      res.json(`Note added successfully`)
+    } else {
+      res.error('Error in adding note')
+    }
 })
 
 module.exports = notesRouter
+
+
